49ed087e8c154f521b8012309f88ce52e92620c1
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2023-03-21 09:12:10 +0100

    Added pictures and stuff from fake store api first

---
+++ Javascript.js
@@ -0,0 +1,24 @@
+const productGrid = document.getElementById("product-grid");
+
+        fetch("Fakestore.json")
+          .then(response => response.json())
+          .then(products => {
+            products.forEach(product => {
+              const div = document.createElement("div");
+              div.classList.add("col-sm-4");
+              div.innerHTML = `
+                <div class="card h-100">
+                  <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                  <div class="card-body">
+                    <h5 class="card-title">${product.title}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <p class="card-text fw-bold">$${product.price}</p>
+                  </div>
+                </div>
+              `;
+              productGrid.appendChild(div);
+            });
+          })
+          .catch(error => {
+            console.error("Error fetching products:", error);
+          });
