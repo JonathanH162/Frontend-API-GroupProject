ea6188c4596febc716e582746b1f961d0fd533d7
Ling Wang <amyling.wang@gmail.com>
2023-03-22 00:13:53 +0100

    added javascript code for butten submit

--- Javascript.js
+++ Javascript.js
@@ -13,68 +13,41 @@ const productGrid = document.getElementById("product-grid");
                     <h5 class="card-title">${product.title}</h5>
                     <p class="card-text">${product.description}</p>
                     <p class="card-text fw-bold">$${product.price}</p>
-                    <a href="purchase.html" class="btn btn-success" >BUY</a>
+                    <a href="#" class="btn btn-success" >BUY</a>
                   </div>
                 </div>`;
-        productGrid.appendChild(div);
-        const buyButton = div.querySelector('.buy-button');
-        if (buyButton) {
-          buyButton.addEventListener('click', (event) => {
-            event.preventDefault(); // Prevent default behavior of anchor tag
+                productGrid.appendChild(div);
 
-            document.addEventListener("click", function(event) {
-                console.log(event.target);
-              });
+                const buyButtons = document.querySelectorAll(".btn-success");
+                console.log(buyButtons)
+                buyButtons.forEach((button) => {button.addEventListener("click", (event)=> {event.preventDefault(); //förhindra sidan laddas om när användaren klickar på länken
+                
+                //hämtar den närmaste parent till knappen som har klassen card
+                const card = button.closest(".card");
+                //spara all info från det klickade kortet 
+                const title = card.querySelector(".card-title").textContent;
+                const description = card.querySelector(".card-text").textContent;
+                const price = card.querySelector(".card-text.fw-bold").textContent;
+                //spara all info i en sträng, änvander encodeURIComponent() för att få fram specialtecken i strängen
+                const queryString = `title=${encodeURIComponent(title)}
+                        &description=${encodeURIComponent(description)}
+                        &price=${encodeURIComponent(price)}`;
+                //skapa en URL med query stringen
+                const url = `purchase.html?${queryString}`;
+                //öppna nya sida med window.location.href
+                window.location.href = url;
+    
+                });
+            })
 
-            console.log(event.currentTarget)
-
-            console.log(event.currentTarget.dataset.product)
-
-            const productData = JSON.parse(event.currentTarget.dataset.product);
-            console.log(productData)
-            localStorage.setItem('productData', JSON.stringify(productData));
-            window.open('purchase.html', '_blank');
-          });
-        }
-      
-         });}).catch(error => {
+            });
+        }).catch(error => {
             console.error("Error fetching products:", error);
         });
         
           
 
-/*const buyButtons = document.querySelectorAll(".btn-success");
-console.log(buyButtons)
-buyButtons.forEach((button) => {button.addEventListener("click", (event)=> {event.preventDefault(); //förhindra sidan laddas om när användaren klickar på länken
 
-    const card = button.closest(".card");
-    //spara all info från det klickade kortet 
-    const title = card.querySelector(".card-title").textContent;
-    const description = card.querySelector(".card-text").textContent;
-    const price = card.querySelector(".card-text.fw-bold").textContent;
-    //spara all info i en sträng, änvander encodeURIComponent() för att få fram specialtecken i strängen
-    const queryString = `title = ${encodeURIComponent(title)}
-                        &description=${encodeURIComponent(description)}
-                        &price=${encodeURIComponent(price)}`;
-    //skapa en URL med query stringen
-    const url = `purchase.html?${queryString}`;
-    //öppna nya sida med window.location.href
-    window.location.href = url;
-    
-    });
-})
 
-<script>
-        const urlParams = new URLSearchParams(window.location.search);
-        const productTitle = urlParams.get('title');
-        const productDescription = urlParams.get('description');
-        const productPrice = urlParams.get('price');
 
-        document.getElementById("product-title").value = productTitle;
-        document.getElementById("product-description").value = productDescription;
-        document.getElementById("product-price").value = productPrice;
-
-        
-    </script>
-*/
         
