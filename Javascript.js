44e4233b12335bb9a1ec7d000c52c03b830b9953
Ling Wang <amyling.wang@gmail.com>
2023-03-21 23:04:19 +0100

    added eventlistener in javascript.js

--- Javascript.js
+++ Javascript.js
@@ -15,29 +15,66 @@ const productGrid = document.getElementById("product-grid");
                     <p class="card-text fw-bold">$${product.price}</p>
                     <a href="purchase.html" class="btn btn-success" >BUY</a>
                   </div>
-                </div>
-              `;
-              productGrid.appendChild(div);
-            });
-          })
-          .catch(error => {
-            console.error("Error fetching products:", error);
-          });
+                </div>`;
+        productGrid.appendChild(div);
+        const buyButton = div.querySelector('.buy-button');
+        if (buyButton) {
+          buyButton.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent default behavior of anchor tag
 
-const buyButtons = document.querySelectorAll(".btn-success");
-buyButtons.forEach((button) => {button.addEventListener("click", (event)=> {event.preventDefault();
+            document.addEventListener("click", function(event) {
+                console.log(event.target);
+              });
+
+            console.log(event.currentTarget)
+
+            console.log(event.currentTarget.dataset.product)
+
+            const productData = JSON.parse(event.currentTarget.dataset.product);
+            console.log(productData)
+            localStorage.setItem('productData', JSON.stringify(productData));
+            window.open('purchase.html', '_blank');
+          });
+        }
+      
+         });}).catch(error => {
+            console.error("Error fetching products:", error);
+        });
+        
+          
+
+/*const buyButtons = document.querySelectorAll(".btn-success");
+console.log(buyButtons)
+buyButtons.forEach((button) => {button.addEventListener("click", (event)=> {event.preventDefault(); //förhindra sidan laddas om när användaren klickar på länken
 
     const card = button.closest(".card");
-    const title = card.querySelector(".card-title".textContent);
+    //spara all info från det klickade kortet 
+    const title = card.querySelector(".card-title").textContent;
     const description = card.querySelector(".card-text").textContent;
-    const price = card.querySelector(".card-text. fw-bold").textContent;
-
+    const price = card.querySelector(".card-text.fw-bold").textContent;
+    //spara all info i en sträng, änvander encodeURIComponent() för att få fram specialtecken i strängen
     const queryString = `title = ${encodeURIComponent(title)}
                         &description=${encodeURIComponent(description)}
                         &price=${encodeURIComponent(price)}`;
+    //skapa en URL med query stringen
     const url = `purchase.html?${queryString}`;
+    //öppna nya sida med window.location.href
     window.location.href = url;
-    console.log(queryString)
+    
     });
 })
+
+<script>
+        const urlParams = new URLSearchParams(window.location.search);
+        const productTitle = urlParams.get('title');
+        const productDescription = urlParams.get('description');
+        const productPrice = urlParams.get('price');
+
+        document.getElementById("product-title").value = productTitle;
+        document.getElementById("product-description").value = productDescription;
+        document.getElementById("product-price").value = productPrice;
+
+        
+    </script>
+*/
         
