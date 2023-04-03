aea29846353e9a8e2ced6f62da5996eda16acb64
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2023-04-03 11:27:22 +0200

    Fixed HTML validation to also use JS validation in contact file. Added new Function: validateForm()

--- Javascript.js
+++ Javascript.js
@@ -214,6 +214,43 @@ function confirmation() {
   document.getElementById("image").src = sessionStorage.getItem('image')
   document.getElementById("tack").textContent = "Tack för att du handlat hos oss, en kopia av bekräftelsen har skickats till " + sessionStorage.getItem("email") + "!"
 }
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let nameError = document.getElementById("nameError");
+  let emailError = document.getElementById("emailError");
+  let messageError = document.getElementById("messageError");
+  let valid = true;
+
+  // Validate name
+  if (name.length < 1 || name.length > 50) {
+    nameError.innerText = "Name must be between 1 and 50 characters";
+    valid = false;
+  } else {
+    nameError.innerText = "";
+  }
+
+  // Validate email
+  if (!email.includes("@") || !email.endsWith(".com") || email.indexOf('@') > email.indexOf('.com') - 1) {
+      emailError.innerText = "Invalid email address";
+      valid = false;
+    } else {
+      emailError.innerText = "";
+    }
+    
+
+  // Validate message
+  if (message.length < 1) {
+    messageError.innerText = "Message cannot be empty";
+    valid = false;
+  } else {
+    messageError.innerText = "";
+  }
+
+  return valid;
+}
   
 
 
